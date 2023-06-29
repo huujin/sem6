426f0b8791cf12062b78bcf6404cbe012838b769
huujin <alexaf_2004@mail.ru>
2023-06-29 22:17:11 +0300

    all_sorts

--- sem6_4.c
+++ sem6_4.c
@@ -88,6 +88,82 @@ void merge_sort(int mas[], int left, int right) {
         merge(mas, left, mid, right); 
     } 
 } 
+// ---------------------------------------------------------
+
+void radixSort(int *arr, int start, int end) 
+{
+    int radix_base = 10; // Основание системы счисления
+    int max_num = arr[0]; // Максимальное число в массиве
+    for (int i = start; i < end; ++i) // Поиск максимального числа в массиве
+        if (max_num < arr[i]) max_num = arr[i];
+    int radx = 0;
+    while (max_num != 0) 
+    { 
+        radx++;
+        max_num /= radix_base;
+    }
+    int *base_arr = malloc(sizeof(int) * radix_base); // Массив для хранения колва чисел в каждом отсеке массива по разрядам
+    for (int i = 0; i < radix_base; ++i) base_arr[i] = 0; // Заполняем его нулями
+    int **digits_arr = malloc(sizeof(int*) * radix_base); // Массив массивов с числами, по разрядам
+    for (int i = 0; i < radix_base; ++i)
+        digits_arr[i] = malloc(sizeof(int) * (end - start + 1));
+
+    for (int i = 0; i < radx; ++i) 
+    {
+        for (int j = start; j < end; ++j) // Разбиваем исходный массив по разрядному числу во вспомогательный массив, попутно считая кол-во чисел в каждой ячейке
+        { 
+            int digit = (arr[j] / pow_(radix_base, i)) % radix_base;
+            digits_arr[digit][base_arr[digit]++] = arr[j];
+        }
+        int count = 0;
+        for (int j = 0; j < radix_base; ++j) // Собираем исходный массив обратно
+        { 
+            for (int k = 0; k < base_arr[j]; ++k) {
+                arr[count++] = digits_arr[j][k];
+            }
+        }
+        for (int j = 0; j < radix_base; ++j) base_arr[j] = 0;
+    }
+
+    for (int i = 0; i < radix_base; ++i)
+        free(digits_arr[i]);
+    free(digits_arr);
+    free(base_arr);
+}
+   
+// ---------------------------------------------------------
+
+
+void qsort (int *mas, int a, int b) {
+    int left = a, right = b;
+    int pivot = mas[(left + right)/2];
+	do {
+		while (mas[left] < pivot) {
+			left++;
+		}
+		while (mas[right] > pivot) {
+			right--;
+		}
+		if (left <= right) {
+			if (mas[left] > mas[right]) {
+				int tmp = mas[left];
+				mas[left] = mas[right];
+				mas[right] = tmp;
+			}
+			left++;
+			if (right > 0) {
+				right--;
+			}
+		}
+	} while (left <= right);
+
+	if (left < b) {
+		qsort(mas, left, b);
+	}
+	if (right > a) {
+		qsort(mas, a, right);
+	}
+}
 
 // ---------------------------------------------------------
 int main(void)
